b12df0b41a4630402adc73e4a22efce92eb5ada9
AtiaABKe <atia.abk@gmail.com>
2026-02-17 23:59:47 +0600

    m

--- q3.py
+++ q3.py
@@ -2,6 +2,10 @@ luggage = [100, 200, 50, 150, 100]
 current_weight = 0
 
 for weight in luggage:
+    item = input(f"Next bag is {weight} kg. Add to elevator? (yes/no): ")
+    if item.lower() != "yes":
+        print("Bag skipped.")
+        continue
     if current_weight + weight > 500:
         print("Elevator Full.")
         break
