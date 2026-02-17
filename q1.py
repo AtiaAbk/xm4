a500feb8791af8432fc91a3ae11f60a11cb767e1
AtiaABKe <atia.abk@gmail.com>
2026-02-17 23:58:17 +0600

    m

--- q1.py
+++ q1.py
@@ -1,4 +1,8 @@
-player = {"name": "Aragon", "level": 10, 
-"health": 50}
-player['health']+=20
-print("new health ", player["health"])
+player = {"name": "Aragon", "level": 10, "health": 50}
+item = input("What item does the player use? ")
+
+if item.lower() == "healing potion":
+    player["health"] += 20
+    print("New health:", player["health"])
+else:
+    print("No effect. Health:", player["health"])
